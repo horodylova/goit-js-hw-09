e6e419e0ebd9c2977136ba3e3d77ab3d368ec908
svitlana-horodylova <horodylova.sv@gmail.com>
2023-10-15 11:17:01 +0100

    update 1-color-switcher-js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,39 @@
+const buttonStart = document.querySelector('[data-start]');
+const buttonEnd = document.querySelector('[data-stop]');
 
+const containerDiv = document.createElement("div");
+containerDiv.appendChild(buttonStart);
+containerDiv.appendChild(buttonEnd);
+
+containerDiv.style.position = "absolute";
+containerDiv.style.left = "50%";
+containerDiv.style.top = "50%";
+
+let intervalId = null; 
+let isColorChanging = false; 
+
+document.body.appendChild(containerDiv);
+
+
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+  }
+
+function changeBackgroundColor() {
+    if(!isColorChanging) {
+        isColorChanging = true;
+    }
+    document.body.style.backgroundColor = getRandomHexColor();
+    isColorChanging = false;
+}
+
+buttonStart.addEventListener("click", () => {
+    buttonStart.disabled = true;
+    intervalId = setInterval(changeBackgroundColor, 1000); // Меняем цвет каждую секунду
+  });
+
+  buttonEnd.addEventListener('click', () => {
+    buttonStart.disabled = false;
+    clearInterval(intervalId);
+  });
