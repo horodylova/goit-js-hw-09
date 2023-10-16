f49e443b6de76c1a368a6b6e1ab7b86ee0d75d7c
svitlana-horodylova <horodylova.sv@gmail.com>
2023-10-16 10:17:15 +0100

    Update-03-promises-js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,23 @@
+import Notiflix from 'notiflix';
+
+const firstDelay = document.querySelector('[name="delay"]');
+console.log(firstDelay);
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
-    // Fulfill
+    
+
   } else {
-    // Reject
+     
+
   }
 }
+
+createPromise(2, 1500)
+  .then(({ position, delay }) => {
+    console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+  })
+  .catch(({ position, delay }) => {
+    console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+  });
