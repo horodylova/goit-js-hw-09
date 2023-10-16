39a899985018bc91eb641e0ce68f6c9bbcdc2893
svitlana-horodylova <horodylova.sv@gmail.com>
2023-10-16 11:31:00 +0100

    new Update-03-promises-js-add-styles

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,23 +1,64 @@
 import Notiflix from 'notiflix';
 
-const firstDelay = document.querySelector('[name="delay"]');
-console.log(firstDelay);
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', async (e) => {
+  e.preventDefault();
+
+  const delay = parseInt(form.elements.delay.value);
+  const step = parseInt(form.elements.step.value);
+  const amount = parseInt(form.elements.amount.value);
+
+  for (let position = 1; position <= amount; position += 1) {
+    const promiseDelay = delay + (position - 1) * step;
+    const promise = createPromise(position, promiseDelay);
+
+    promise
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+  }
+});
 
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    
 
-  } else {
-     
-
-  }
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
 }
 
-createPromise(2, 1500)
-  .then(({ position, delay }) => {
-    console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
-  })
-  .catch(({ position, delay }) => {
-    console.log(`❌ Rejected promise ${position} in ${delay}ms`);
-  });
+//add styles 
+
+const button = form.querySelector('button');
+
+button.style.backgroundColor = '#007bff';
+button.style.color = '#fff';
+button.style.padding = '10px 20px';
+button.style.border = 'none';
+button.style.borderRadius = '5px';
+button.style.cursor = 'pointer';
+
+const inputElements = form.querySelectorAll('input');
+
+inputElements.forEach(input => {
+  input.style.padding = '10px';
+  input.style.margin = '5px 0';
+  input.style.border = '1px solid #ccc';
+  input.style.borderRadius = '5px';
+});
+
+form.style.display = 'inline-block';
+form.style.backgroundColor = '#f0f0f0';
+form.style.padding = '20px';
+form.style.border = '1px solid #ccc';
+
